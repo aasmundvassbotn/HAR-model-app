5cd34dfbafacbe6c48a3dee47e9263956cda87de
669808 <669808@stud.hvl.no>
2025-04-07 12:13:03 +0200

    Load model

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 import streamlit as st
-
-
+import tensorflow as tf
+import keras
+import numpy as np
 
 def main():
     st.title("My Streamlit App")
     st.write("Hello, world!")
+    keras.saving.load_model("bidirectional_model.keras")
     video_file = st.file_uploader("Video file", type="mp4")
 
 if __name__ == "__main__":
