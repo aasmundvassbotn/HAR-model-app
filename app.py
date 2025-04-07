b62030706dea39929cf574b1440877f4a0586c41
669808 <669808@stud.hvl.no>
2025-04-07 10:18:41 +0200

    With file upload

--- app.py
+++ app.py
@@ -1,4 +1,11 @@
 import streamlit as st
 
-st.title("My Streamlit App")
-st.write("Hello, world!")
+
+
+def main():
+    st.title("My Streamlit App")
+    st.write("Hello, world!")
+    video_file = st.file_uploader("Video file", type="mp4")
+
+if __name__ == "__main__":
+    st.main()
