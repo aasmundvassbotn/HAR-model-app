f1aa0be5971e676f0185f02c2507fb48d99a3a3f
669808 <669808@stud.hvl.no>
2025-04-24 11:48:32 +0200

    Bugfix

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ def detect_keypoints(video_file):
       temp_file_path = temp_file.name
 
     # Pass the file-like object directly to the YOLO model
-    results = model.track(source=temp_file_path, save=True, stream=True)
+    results = model.track(source=temp_file_path, save=True, stream=True, save_dir="runs/pose/track2")
     video_keypoints = np.ndarray((128, 17, 2))
     for i, result in enumerate(results):
         frame = result.orig_img  # Use the original frame from the result
@@ -32,7 +32,7 @@ def detect_keypoints(video_file):
             x[i][j*2+1] = keypoint[1]
     x = np.expand_dims(x, axis=0)
 
-    output_folder = "runs/pose/track"
+    output_folder = "runs/pose/track2"
     # Find the most recent file (assuming you want the last run's result)
     video_files = [f for f in os.listdir(output_folder) if f.endswith(('.avi'))]
     video_files.sort(key=lambda x: os.path.getmtime(os.path.join(output_folder, x)))
